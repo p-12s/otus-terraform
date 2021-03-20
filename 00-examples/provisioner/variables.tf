3ca938b610caa5f0b90f1bbc2397956139780ca4
p-12s <mental.notes@yandex.ru>
2021-03-20 14:51:10 +0300

    Added examples folder with first example - provisioner YC

---
+++ 00-examples/provisioner/variables.tf
@@ -0,0 +1,15 @@
+variable "cloud_id" {
+  type = string
+}
+variable "folder_id" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
+variable "yc_token" {
+  type = string
+}
+variable "image_id" {
+  type = string
+}
