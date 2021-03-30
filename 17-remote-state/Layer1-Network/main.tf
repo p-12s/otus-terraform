8dbd6d8437dd43c4f56d5c563023dc17540367c3
p-12s <mental.notes@yandex.ru>
2021-03-30 15:29:26 +0300

    Remote state

---
+++ 17-remote-state/Layer1-Network/main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  # доступы от текущего аккаунта
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+  alias      = "IRE"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "p12s-otus-terraform"
+    key    = "dev/network/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+#==============================================================
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags = {
+    Name = "${var.env}-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.env}-igw"
+  }
+}
+
+
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.env}-puvlic-${count.index + 1}"
+  }
+}
+
+
+resource "aws_route_table" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.env}-route-public-subnets"
+  }
+}
+
+resource "aws_route_table_association" "public_routes" {
+  count          = length(aws_subnet.public_subnets[*].id)
+  route_table_id = aws_route_table.public_subnets.id
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public_subnets[*].id
+}
